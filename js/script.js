e0e9d247f126d77750c40c613fd9d61885909856
Kevin Babcock <kevin@>
2017-10-17 14:19:12 -0700

    create page with all terms, and links to click each term and display definition

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+  $(".javascript").click(function() {
+    $(".javascript .definition").toggle();
+  });
+  $(".operators").click(function() {
+    $(".operators .definition").toggle();
+  });
+
+})
